260baab2e44ea54a8c509f18e801319e833aae6a
Eric Chu <eric@ericchu.net>
2015-12-04 01:48:41 -0800

    Add request timing

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const co = require('co')
 const app = new Koa()
 
 app.use(co.wrap(function *(ctx, next) {
+  var start = new Date()
+  yield next()
+  var end = new Date()
+  var diff = end - start
+  console.log(`${start.toISOString()} ${ctx.method} ${ctx.url} ${ctx.status} ${diff}ms`)
+}))
+
+app.use(co.wrap(function *(ctx, next) {
   ctx.body = 'Hello, World!'
 }))
 
